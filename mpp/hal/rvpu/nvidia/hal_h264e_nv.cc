9897a407db44b7859d5e40aa568414981d49ff55
ziven <ziven@zerone.io>
2020-02-23 11:15:16 +0800

    Fixed problem when setup nvidia cuda input stream

--- mpp/hal/rvpu/nvidia/hal_h264e_nv.cc
+++ mpp/hal/rvpu/nvidia/hal_h264e_nv.cc
@@ -47,7 +47,6 @@ typedef struct hal_nvenc_info_s {
 
 static MPP_RET reinit_nvenc(MppEncCfgSet *cfg, HalNvEncInfo *info)
 {
-    int max_cached_frames;
     int width  = cfg->prep.width;
     int height = cfg->prep.height;
     NV_ENC_BUFFER_FORMAT format = NV_ENC_BUFFER_FORMAT_IYUV; // I420
@@ -60,26 +59,12 @@ static MPP_RET reinit_nvenc(MppEncCfgSet *cfg, HalNvEncInfo *info)
         delete info->encoder;
     }
 
-    NvEncoderOutputInVidMemCuda *encoder = new NvEncoderOutputInVidMemCuda(info->cu_context, width, height, format);
+    NvEncoderOutputInVidMemCuda *encoder = 
+        new NvEncoderOutputInVidMemCuda(info->cu_context, width, height, format);
     if (nullptr == encoder) {
         mpp_err("NvEncoderCuda create failed");
         return MPP_ERR_VPU_CODEC_INIT;
     }
-
-    // info->stream = new NvCUStream(info->cu_context, 1, encoder);
-    CUDA_DRVAPI_CALL(cuCtxPushCurrent(info->cu_context));
-    if (CUDA_SUCCESS != cuStreamCreate(&info->in_stream, CU_STREAM_DEFAULT)) {
-        mpp_err("cuStreamCreate");
-    }
-    // (void)cuStreamCreate(&info->out_stream, CU_STREAM_DEFAULT);
-    info->out_stream = info->in_stream;
-
-    CUDA_DRVAPI_CALL(cuCtxPopCurrent(NULL));
-
-    // Set input and output CUDA streams in driver
-    encoder->SetIOCudaStreams(&info->in_stream, &info->out_stream);
-
-    // info->cu_crc = new NvCUCrc(info->cu_device, encoder->GetOutputBufferSize());
 
     NV_ENC_INITIALIZE_PARAMS param = { 0 };
     NV_ENC_CONFIG encodeConfig = { NV_ENC_CONFIG_VER };
@@ -90,6 +75,29 @@ static MPP_RET reinit_nvenc(MppEncCfgSet *cfg, HalNvEncInfo *info)
     encoder->CreateDefaultEncoderParams(&param, encode_guid, preset_guid);
     encoder->CreateEncoder(&param);
 
+    // Initialize stream
+    // info->stream = new NvCUStream(info->cu_context, 1, encoder);
+    CUDA_DRVAPI_CALL(cuCtxPushCurrent(info->cu_context));
+    if (CUDA_SUCCESS != cuStreamCreate(&info->in_stream, CU_STREAM_DEFAULT)) {
+        mpp_err("cuStreamCreate in_stream");
+    }
+    
+#if 1
+    info->out_stream = info->in_stream;
+#else
+    if (CUDA_SUCCESS != cuStreamCreate(&info->out_stream, CU_STREAM_DEFAULT)) {
+        mpp_err("cuStreamCreate out_stream");
+    }
+#endif
+
+    CUDA_DRVAPI_CALL(cuCtxPopCurrent(NULL));
+
+    // Set input and output CUDA streams in driver
+    encoder->SetIOCudaStreams(&info->in_stream, &info->out_stream);
+
+    // info->cu_crc = new NvCUCrc(info->cu_device, encoder->GetOutputBufferSize());
+
+    // Assign encoder
     info->encoder = encoder;
 
     int frame_size = encoder->GetFrameSize();
@@ -106,6 +114,8 @@ MPP_RET hal_h264e_nv_init(void *hal, MppHalCfg *cfg)
     HalNvEncInfo *info;
     MppEncCfgSet *ecfg = ctx->cfg;
     CUresult ret;
+
+    (void)ecfg;
 
     mpp_log("init %p", ctx);
 
@@ -136,7 +146,9 @@ MPP_RET hal_h264e_nv_init(void *hal, MppHalCfg *cfg)
     cuDeviceGetName(devname, sizeof(devname), info->cu_device);
     mpp_log("GPU in use: [%d] %s", i_gpu, devname);
 
-    cuCtxCreate(&info->cu_context, 0, info->cu_device);
+    if (CUDA_SUCCESS != (ret = cuCtxCreate(&info->cu_context, 0, info->cu_device))) {
+        mpp_err("retcuCtxCreate: %d", ret);
+    }
 
 #endif /* _VPU_STUB_ */
 
@@ -183,12 +195,10 @@ MPP_RET hal_h264e_nv_deinit(void *hal)
 MPP_RET hal_h264e_nv_gen_regs(void *hal, HalTaskInfo *task)
 {
     H264eHalContext *ctx = (H264eHalContext *)hal;
-    
-    //MppEncPrepCfg  *prep  = &ctx->cfg->prep;
-    //MppEncCodecCfg *codec = &ctx->cfg->codec;
     (void)ctx;
+    (void)task;
 
-    mpp_log("gen_regs v:%u\n", task->enc.valid);
+    // mpp_log("gen_regs v:%u\n", task->enc.valid);
 
 #ifdef _VPU_STUB_
     // in container, set task regs
@@ -202,16 +212,13 @@ MPP_RET hal_h264e_nv_gen_regs(void *hal, HalTaskInfo *task)
 MPP_RET hal_h264e_nv_start(void *hal, HalTaskInfo *task)
 {
     H264eHalContext *ctx = (H264eHalContext *)hal;
-    HalNvEncInfo *info = (HalNvEncInfo *)ctx->extra_info;
+    HalNvEncInfo   *info = (HalNvEncInfo *)ctx->extra_info;
+
+    //mpp_log("start v:%u\n", task->enc.valid);
 
 #ifndef _VPU_STUB_
     // in host, send task to nvenc
-
-    MppBuffer   input  = task->enc.input;
-  //MppBuffer   output = task->enc.output;
-
-    mpp_log("start v:%u\n", task->enc.valid);
-
+    MppBuffer input = task->enc.input;
     NvEncoderCuda *encoder = static_cast<NvEncoderCuda *>(info->encoder);
     const NvEncInputFrame *input_frame = encoder->GetNextInputFrame();
 
@@ -239,15 +246,14 @@ MPP_RET hal_h264e_nv_start(void *hal, HalTaskInfo *task)
 MPP_RET hal_h264e_nv_wait(void *hal, HalTaskInfo *task)
 {
     H264eHalContext *ctx = (H264eHalContext *)hal;
-    HalNvEncInfo *info = (HalNvEncInfo *)ctx->extra_info;
+    HalNvEncInfo   *info = (HalNvEncInfo *)ctx->extra_info;
+
+    // mpp_log("wait w:%u v:%u\n", ctx->cfg->prep.hor_stride, task->enc.valid);
 
 #ifndef _VPU_STUB_
     // in host, wait nvenc response
-    MppBuffer   output = task->enc.output;
-
-    // mpp_log("wait w:%u v:%u\n", ctx->cfg->prep.hor_stride, task->enc.valid);
+    MppBuffer output = task->enc.output;
     void *pbuf = mpp_buffer_get_ptr(output);
-
     std::vector<NV_ENC_OUTPUT_PTR> packets;
     NvEncoderOutputInVidMemCuda *encoder = static_cast<NvEncoderOutputInVidMemCuda *>(info->encoder);
     encoder->EncodeFrame(packets);
@@ -255,10 +261,18 @@ MPP_RET hal_h264e_nv_wait(void *hal, HalTaskInfo *task)
     uint32_t bsize = 0;
     if (packets.size() > 0) {
         // only use first frame
-        NV_ENC_OUTPUT_PTR ptr = packets[0];
-        cuMemcpyDtoH(pbuf, (CUdeviceptr)ptr, encoder->GetOutputBufferSize());
-        NV_ENC_ENCODE_OUT_PARAMS *eparm = reinterpret_cast<NV_ENC_ENCODE_OUT_PARAMS *>(pbuf);
-        bsize = eparm->bitstreamSizeInBytes;
+        NV_ENC_OUTPUT_PTR pkt_ptr = packets[0];
+        NV_ENC_ENCODE_OUT_PARAMS *pkt_parm = static_cast<NV_ENC_ENCODE_OUT_PARAMS *>(pbuf);
+
+        // mpp_log("EncodeFrame got %d packets\n", packets.size());
+
+        cuCtxPushCurrent(info->cu_context);
+        // cuMemcpyDtoH(pbuf, (CUdeviceptr)pkt_ptr, encoder->GetOutputBufferSize());
+        cuMemcpyDtoH(pbuf, (CUdeviceptr)pkt_ptr, sizeof(NV_ENC_ENCODE_OUT_PARAMS));
+        if (0 < (bsize = pkt_parm->bitstreamSizeInBytes)) {
+            cuMemcpyDtoH(pbuf, (CUdeviceptr)pkt_ptr + sizeof(NV_ENC_ENCODE_OUT_PARAMS), bsize);
+        }
+        cuCtxPopCurrent(NULL);
     }
 
     if (bsize > 0) {
@@ -312,32 +326,26 @@ MPP_RET hal_h264e_nv_control(void *hal, RK_S32 cmd, void *arg)
     switch (cmd) {
     case MPP_ENC_SET_PREP_CFG:
         if (info) {
-            MppEncCfgSet *ecfg = ctx->set;
-            // x264_param_t *param = &info->param;
-            // param->i_width  = ecfg->prep.width;
-            // param->i_height = ecfg->prep.height;
+            // MppEncCfgSet *ecfg = ctx->set;
+            // ecfg->prep.width;
+            // ecfg->prep.height;
         }
         break;
     
     case MPP_ENC_SET_RC_CFG:
         if (info) {
-            MppEncCfgSet *ecfg = ctx->set;
-            // x264_param_t *param = &info->param;
-            // param->i_fps_den = ecfg->rc.fps_in_denorm;
-            // param->i_fps_num = ecfg->rc.fps_in_num;
+            // MppEncCfgSet *ecfg = ctx->set;
+            // ecfg->rc.fps_in_denorm;
+            // ecfg->rc.fps_in_num;
         }
         break;
 
     case MPP_ENC_SET_CODEC_CFG:
         if (info) {
             MppEncCfgSet *ecfg = ctx->set;
-
-            // TODO
-            (void)ecfg;
             // ecfg->codec.h264.qp_init
             // ecfg->codec.h264.profile
             // ecfg->codec.h264.level
-
             ret = reinit_nvenc(ecfg, info);
         }
         break;
